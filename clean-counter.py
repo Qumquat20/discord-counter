96b395d5450076efba9f4558cca0abef6ebb4ad3
Qumquat20 <ay4-qumquat@protonmail.com>
2020-07-24 22:17:11 +0700

    On branch master Initial commit Changes to be committed: 	new file:   .gitignore 	new file:   clean-counter.py 	new file:   nword-counter.py

---
+++ clean-counter.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import discord
+import time
+import asyncio
+from collections import OrderedDict
+from discord.ext import commands
+import os.path
+import pickle
+
+
+token = open("token.txt","r").readline()
+client = commands.Bot(command_prefix = '!')
+
+def get_int(user):
+	points = pickle.load(open('points.p','rb'))
+	if user in points:
+		global score
+		points = pickle.load(open('points.p','rb'))
+		score = points[user]
+		return score
+	else:
+		pass
+            
+def write_int(user):
+	points = pickle.load(open('points.p','rb'))
+	if user not in points:
+		points.update( {user: 1} )
+		pickle.dump(points,open('points.p','wb'))
+	else:
+		points[user] = int(points[user]) + 1
+		pickle.dump(points,open('points.p','wb'))
+		print(points[user])
+
+def rw(user):
+    get_int(user)
+    write_int(user)
+
+def rem_dup(word):
+	return "".join(OrderedDict.fromkeys(word))
+
+def get_ncount(user):
+	get_int('{}'.format(str(user)))
+	print(score)
+	global msg
+	msg = 'This user has said {} Words'.format(score)
+	return msg
+
+@client.command()
+async def test(ctx):
+	await ctx.send('Alive')
+
+@client.command()
+async def count(ctx, user : discord.User):
+	uid = user.id
+	get_ncount(uid)
+	await ctx.send(msg)
+
+
+@client.event
+async def on_ready():
+	print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	await client.process_commands(message)
+
+	print(message.content)
+	msgcon = message.content.casefold()
+	print(msgcon)
+	msgcon = msgcon.replace(" ","")
+	print(msgcon)
+	msgcon = rem_dup(msgcon)
+	print(msgcon)
+
+	if '' in msgcon or '' in msgcon or '' in msgcon or msgcon == '':
+		print('Detected')
+		print(message.author, message.author.id,'\n')
+		uid = message.author.id
+		rw(uid)
+		
+
+client.run(token)
